876c8d1ff871635a2b151fddc64e9d84f05a7a9f
Calvin Teixeira <calvin.teixeira@avmb.com.br>
2022-06-14 14:31:52 -0300

    Criando métodos reutilizáveis para validação

--- src/services/utils/validateFields.js
+++ src/services/utils/validateFields.js
@@ -1,35 +1,53 @@
 export default function validateFields(fields) {
   const errors = {};
+  const validateMethods = {
+    isEmptyField: (field) => {
+      if (!field) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    mismatchPattern: (pattern, field) => {
+      if (!pattern.test(field)) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    mismatchLength: (min, max, field) => {
+      if (field.length < min || field.length > max) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  };
 
-  // VALIDANDO O CAMPO NOME
+  // VERIFICAÇÃO DO CAMPO NOME
   if (fields.hasOwnProperty("name")) {
-    if (!fields.name) {
-      errors.name = "Campo obrigatório!";
-    } else if (fields.name < 4 || fields.name > 20) {
-      errors.name = "O campo precisa ter entre 4 e 20 caracteres";
-    } else if (!/\s\w/g.test(fields.name)) {
-      errors.name = "Digite seu nome completo";
+    if (validateMethods.isEmptyField(fields.name)) {
+      errors.nomeUsuario = "Campo obrigatório!";
+    } else if (validateMethods.mismatchLength(4, 20, fields.name)) {
+      errors.nomeUsuario = "O campo precisa ter entre 4 e 20 caracteres";
+    } else if (validateMethods.mismatchPattern(/\s\w{3}/g, fields.name)) {
+      errors.nomeUsuario = "Digite seu nome completo";
     }
   }
 
-  // VALIDANDO O CAMPO CPF
+  // VERIFICAÇÃO DO CAMPO CPF
   if (fields.hasOwnProperty("cpf")) {
-    if (!fields.cpf) {
+    if (validateMethods.isEmptyField(fields.cpf)) {
       errors.cpf = "Campo obrigatório!";
-    }
-  }
-
-  // VALIDANDO O CAMPO USERNAME
-  if (fields.hasOwnProperty("username")) {
-    if (!fields.username) {
-      errors.username = "Campo obrigatório!";
-    }
-  }
-
-  // VALIDANDO O CAMPO PASSWORD
-  if (fields.hasOwnProperty("password")) {
-    if (!fields.password) {
-      errors.password = "Campo obrigatório!";
+    } else if (validateMethods.mismatchLength(14, 14, fields.cpf)) {
+      errors.cpf = "Digite seu CPF completo";
+    } else if (
+      validateMethods.mismatchPattern(
+        /[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}/,
+        fields.cpf
+      )
+    ) {
+      errors.cpf = "Campo em formato inválido!";
     }
   }
 
