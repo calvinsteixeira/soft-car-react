c173cace28c558e5c97254032aa0e57f91897bd2
Calvin <calvingsx@gmail.com>
2022-06-24 00:20:48 -0300

    Realizando ajustes na renderização das telas

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,16 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { LoginView } from "../views/pages/LoginView";
 import { RegisterView } from "../views/pages/RegisterView";
 import { HomeView } from "../views/pages/HomeView";
 
 export function Router() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginView />} />
-        <Route path="/novo-cadastro" element={<RegisterView />} />
-        <Route path="/meus-carros" element={<HomeView />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LoginView />} />
+      <Route path="/novo-cadastro" element={<RegisterView />} />
+      <Route path="/meus-carros" element={<HomeView />} />
+    </Routes>
   );
 }
